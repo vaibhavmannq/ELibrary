c520460473830454ddf7b8150d9542252e817f5a
vaibhavmannq <vaibhavmann.10@gmail.com>
2024-08-12 14:04:25 +0530

    setup express server

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+// Routes
+// Http Methods: GET, POST, PUT, PATCH, DELETE
+app.get('/', (req, res, next) => {
+    res.json({'message': 'Welcome to ELibrary API'});
+})
+
+export default app;
