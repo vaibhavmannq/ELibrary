138771b24ff954d816f86928bf4f2855b129349e
vaibhavmannq <vaibhavmann.10@gmail.com>
2024-08-12 14:56:05 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
-import app from './src/app'
+import app from "./src/app";
+import { config } from "./src/config/config";
 
 const startServer = () => {
-    const port = process.env.PORT || 3000;
+  const port = config.port || 3000;
 
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 };
 
 startServer();
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
 
 // Routes
 // Http Methods: GET, POST, PUT, PATCH, DELETE
-app.get('/', (req, res, next) => {
-    res.json({'message': 'Welcome to ELibrary API'});
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to ELibrary API" });
+});
 
 export default app;
