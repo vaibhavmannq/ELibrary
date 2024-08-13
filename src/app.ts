eeed736cde91c19abf8eb919be648c76d94f6b68
vaibhavmannq <vaibhavmann.10@gmail.com>
2024-08-13 15:43:11 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app = express();
 
 // Routes
 // Http Methods: GET, POST, PUT, PATCH, DELETE
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to ELibrary API" });
 });
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 export default app;
