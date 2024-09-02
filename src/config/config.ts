b8b240264798b799932b61fdaae7f1c31d5e22b2
vaibhavmannq <vaibhavmann.10@gmail.com>
2024-09-02 23:06:39 +0530

    add cors support

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,6 +9,7 @@ const _config = {
   cloudinaryCloud: process.env.CLOUDINARY_CLOUD,
   cloudinaryApikey: process.env.CLOUDINARY_API_KEY,
   cloudinarySecret: process.env.CLOUDINARY_API_SECRET,
+  frontendDomain: process.env.FRONTEND_DOMAIN,
 };
 
 export const config = Object.freeze(_config);
