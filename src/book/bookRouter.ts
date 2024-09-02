6104e12176a24dd48d5974aff497bb1bf374b961
vaibhavmannq <vaibhavmann.10@gmail.com>
2024-09-02 13:38:43 +0530

    add jwt verification middleware

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import express from "express";
 import { createBook } from "./bookController";
 import multer from "multer";
+import authenticate from "../middleware/authenticate";
 
 const bookRouter = express.Router();
 
@@ -12,9 +13,14 @@ const upload = multer({
 });
 
 // routes
-bookRouter.post("/", upload.fields([
+bookRouter.post(
+  "/",
+  authenticate,
+  upload.fields([
     { name: "coverImage", maxCount: 1 },
     { name: "file", maxCount: 1 },
-]), createBook);
+  ]),
+  createBook
+);
 
 export default bookRouter;
